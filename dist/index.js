0619d2d2233cdd420100270de239fbfa32953957
Guillaume Démurgé <gdemurge@gmail.com>
2017-12-23 12:45:28 +0100

    Fixed runtime errors again

--- dist/index.js
+++ dist/index.js
@@ -112,56 +112,6 @@ var utils = Object.freeze({
 	mask: mask
 });
 
-var objectDestructuringEmpty = function (obj) {
-  if (obj == null) throw new TypeError("Cannot destructure undefined");
-};
-
-
-
-
-
-
-
-
-
-var slicedToArray = function () {
-  function sliceIterator(arr, i) {
-    var _arr = [];
-    var _n = true;
-    var _d = false;
-    var _e = undefined;
-
-    try {
-      for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) {
-        _arr.push(_s.value);
-
-        if (i && _arr.length === i) break;
-      }
-    } catch (err) {
-      _d = true;
-      _e = err;
-    } finally {
-      try {
-        if (!_n && _i["return"]) _i["return"]();
-      } finally {
-        if (_d) throw _e;
-      }
-    }
-
-    return _arr;
-  }
-
-  return function (arr, i) {
-    if (Array.isArray(arr)) {
-      return arr;
-    } else if (Symbol.iterator in Object(arr)) {
-      return sliceIterator(arr, i);
-    } else {
-      throw new TypeError("Invalid attempt to destructure non-iterable instance");
-    }
-  };
-}();
-
 var lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt. ut labore et dolore magna aliqua. Enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip commodo consequat. Duis aute irure dolor reprehenderit voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim est laborum.'.toLowerCase().split(/\s*[,. ]\s*/).filter(function (w) {
   return w !== undefined && w.length > 0;
 });
@@ -197,15 +147,13 @@ function sentences() {
       _ref2$min = _ref2.min,
       min = _ref2$min === undefined ? 2 : _ref2$min,
       _ref2$max = _ref2.max,
-      max = _ref2$max === undefined ? 4 : _ref2$max,
-      _ref2$words = _ref2.words,
-      words = _ref2$words === undefined ? {} : _ref2$words;
+      max = _ref2$max === undefined ? 4 : _ref2$max;
 
   var result = [];
   var count = Math.max(1, integer({ min: min, max: max }));
 
   while (count > 0) {
-    result.push(words(words));
+    result.push(words());
     count--;
   }
 
@@ -213,22 +161,16 @@ function sentences() {
 }
 
 function paragraphes() {
-  var _ref3 = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-
-  objectDestructuringEmpty(_ref3.sentences);
-
   var result = [];
   var count = Math.max(1, integer({ min: min, max: max }));
 
   while (count > 0) {
-    result.push(sentences(sentences));
+    result.push(sentences());
     count--;
   }
 
   return result.join('\n');
 }
-
-
 
 var lorem$1 = Object.freeze({
 	word: word,
@@ -305,6 +247,56 @@ var images = Object.freeze({
 	realImageUrl: realImageUrl,
 	avatarUrl: avatarUrl
 });
+
+var objectDestructuringEmpty = function (obj) {
+  if (obj == null) throw new TypeError("Cannot destructure undefined");
+};
+
+
+
+
+
+
+
+
+
+var slicedToArray = function () {
+  function sliceIterator(arr, i) {
+    var _arr = [];
+    var _n = true;
+    var _d = false;
+    var _e = undefined;
+
+    try {
+      for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) {
+        _arr.push(_s.value);
+
+        if (i && _arr.length === i) break;
+      }
+    } catch (err) {
+      _d = true;
+      _e = err;
+    } finally {
+      try {
+        if (!_n && _i["return"]) _i["return"]();
+      } finally {
+        if (_d) throw _e;
+      }
+    }
+
+    return _arr;
+  }
+
+  return function (arr, i) {
+    if (Array.isArray(arr)) {
+      return arr;
+    } else if (Symbol.iterator in Object(arr)) {
+      return sliceIterator(arr, i);
+    } else {
+      throw new TypeError("Invalid attempt to destructure non-iterable instance");
+    }
+  };
+}();
 
 var firstnamesFemale = ['Louise', 'Camille', 'Emma', 'Inès', 'Chloe', 'Sarah', 'Alice', 'Lea', 'Juliette', 'Jeanne', 'Eva', 'Clara', 'Lina', 'Anna', 'Charlotte', 'Mathilde', 'Marie', 'Manon', 'Lucie', 'Anaïs', 'Jade', 'Zoe', 'Nina', 'Lou', 'Clemence', 'Pauline', 'Lisa', 'Adèle', 'Gabrielle', 'Romane', 'Julia', 'Victoria', 'Emilie', 'Rose', 'Julie', 'Margaux', 'Lena', 'Victoire', 'Lola', 'Valentine', 'Agathe', 'Alix', 'Elisa', 'Elise', 'Margot', 'Yasmine', 'Laura', 'Noa', 'Sofia', 'Noemie', 'Heloïse', 'Sara', 'Apolline', 'Salome', 'Diane', 'Maya', 'Ambre', 'Elsa', 'Josephine', 'Capucine', 'Ava', 'Sasha', 'Eleonore', 'Iris', 'Fatoumata', 'Alicia', 'Eden', 'Mila', 'Nour', 'Andrea', 'Garance', 'Aya', 'Violette', 'Constance', 'Justine', 'Melissa', 'Oceane', 'Olivia', 'Esther', 'Ines', 'Luna', 'Suzanne', 'Mariam', 'Maëlys', 'Myriam', 'Sophie', 'Celia', 'Lila', 'Madeleine', 'Marion', 'Anouk', 'Stella', 'Eloïse', 'Charlie', 'Leonie', 'Chiara', 'Carla', 'Blanche', 'Faustine', 'Claire', 'Lily', 'Roxane', 'Maëlle', 'Alexandra', 'Elena', 'Daphne', 'Clementine', 'Lise', 'Ella', 'Aminata', 'Romy', 'Aïcha', 'Kenza', 'Sophia', 'Lilou', 'Marine', 'Celeste', 'Lyna', 'Paloma', 'Lucile', 'Salma', 'Maïa', 'Melina', 'Sixtine', 'Amelie', 'Imane', 'Eve', 'Anaëlle', 'Lilia', 'Selma', 'Alma', 'Amandine', 'Eugenie', 'Louna', 'Celine', 'Raphaëlle', 'Ninon', 'Naomi', 'Manel', 'Elodie', 'Jasmine', 'Solène', 'Helène', 'Hanna', 'Angèle', 'Rachel', 'Tess', 'Thaïs', 'Albane', 'Berenice', 'Morgane', 'Sirine', 'Maria', 'Hortense', 'Chloé', 'Ana', 'Awa', 'Mia', 'Rebecca', 'Marguerite', 'Giulia', 'Alexia', 'Aurore', 'Isaure', 'Estelle', 'Philippine', 'Hannah', 'Nora', 'Audrey', 'Shirel', 'Clarisse', 'Amina', 'Lana', 'Assia', 'Rania', 'Hawa', 'Judith', 'Penelope', 'Rosalie', 'Alienor', 'Elea', 'Flora', 'Axelle', 'Farah', 'Lili', 'Louison', 'Coline', 'Astrid', 'Caroline', 'Fanny', 'Angelina', 'Zelie', 'Maïssa', 'Fatima', 'Jessica', 'Maryam', 'Leane', 'Dina', 'Bintou', 'Candice', 'Maud', 'Cassandre', 'Elisabeth', 'Ariane', 'Mina', 'Inaya', 'Elia', 'Shana', 'Lara', 'Maeva', 'Kelly', 'Syrine', 'Laure', 'Fanta', 'Charline', 'Louisa', 'Flore', 'Melanie', 'Mona', 'Amira', 'Enora', 'Lison', 'Sonia', 'Maïmouna', 'Cecile', 'Colombe', 'Camelia', 'Ludivine', 'Brune', 'Naomie', 'Laetitia', 'Clotilde', 'Marwa', 'Sabrina', 'Prune', 'Irène', 'Sidonie', 'Bianca', 'Fatou', 'Yaël', 'Oumou', 'Anastasia', 'Chaïma', 'Alya', 'Tessa', 'Emmy', 'Anissa', 'Mahaut', 'Livia', 'Alyssa', 'Fleur', 'Domitille', 'Vanessa', 'Pia', 'Léa', 'Lydia', 'Emy', 'Tara', 'Hana', 'Oriane', 'Diana', 'Isabelle', 'Cassandra', 'Asma', 'Marianne', 'Emmanuelle', 'Cleo', 'Mariama', 'Aliya', 'Leïla', 'Maylis', 'Marilou', 'Paola', 'Dounia', 'Ashley', 'Amel', 'Bertille', 'Ilona', 'Ethel', 'Aïssatou', 'Leonore', 'Louane', 'Philomène', 'Grace', 'Maïlys', 'Marina', 'Thelma', 'Lilas', 'Mae', 'Olympe', 'Elina', 'Assa', 'Amelia', 'Meline', 'Talia', 'Loane', 'Sana', 'Joséphine', 'Anne', 'Helena'];
 var firstnamesMale = ['Gabriel', 'Alexandre', 'Arthur', 'Adam', 'Raphaël', 'Louis', 'Paul', 'Antoine', 'Maxime', 'Thomas', 'Victor', 'Lucas', 'Jules', 'Nathan', 'Hugo', 'Sacha', 'Mohamed', 'Enzo', 'Gaspard', 'Clement', 'Samuel', 'Ethan', 'Leo', 'Adrien', 'Martin', 'Rayan', 'Baptiste', 'Yanis', 'Simon', 'Joseph', 'Mathis', 'Oscar', 'Alexis', 'Pierre', 'Noah', 'Axel', 'Theo', 'Romain', 'Valentin', 'Augustin', 'Julien', 'Nicolas', 'Maxence', 'Noe', 'Benjamin', 'Quentin', 'Tom', 'Charles', 'David', 'Jean', 'Eliott', 'Aaron', 'Elias', 'Matthieu', 'Tristan', 'Timothee', 'Mathieu', 'Matteo', 'William', 'Amine', 'Felix', 'Côme', 'Ruben', 'Ulysse', 'Antonin', 'Guillaume', 'Aurelien', 'Robin', 'Maël', 'Kevin', 'Mehdi', 'Ismaël', 'Liam', 'Gabin', 'Noam', 'Evan', 'Marius', 'Mathias', 'Isaac', 'Samy', 'Ibrahim', 'Basile', 'Daniel', 'Emile', 'Lucien', 'Elie', 'Rayane', 'Leonard', 'Thibault', 'Achille', 'Leon', 'Edouard', 'Hadrien', 'Lenny', 'Matheo', 'Malo', 'Titouan', 'Solal', 'Joshua', 'Max', 'Gregoire', 'Vincent', 'Youssef', 'Luca', 'Esteban', 'Yacine', 'Florian', 'Corentin', 'Amaury', 'Edgar', 'Yassine', 'Roman', 'Milo', 'Octave', 'Hector', 'Ahmed', 'Hippolyte', 'Dylan', 'Ayoub', 'Emmanuel', 'Nolan', 'Abel', 'Yann', 'Mamadou', 'Ali', 'Anatole', 'Etienne', 'Naël', 'Anis', 'Erwan', 'Stanislas', 'Theodore', 'Armand', 'Moussa', 'Marc', 'Ilyes', 'Theophile', 'Lorenzo', 'Ryan', 'Diego', 'Pablo', 'Nils', 'Rafael', 'Sofiane', 'Alex', 'Vadim', 'Amir', 'Kylian', 'Jeremy', 'Gustave', 'Arsène', 'Loïc', 'Leopold', 'Cesar', 'Elliot', 'Mateo', 'Wassim', 'Nino', 'Anthony', 'Jonathan', 'Anton', 'François', 'Dorian', 'Eliot', 'Remi', 'Milan', 'Mathys', 'Hamza', 'Marin', 'Henri', 'Maximilien', 'Ilian', 'Sami', 'Nathanaël', 'Nassim', 'Issa', 'Zakaria', 'Luc', 'Joachim', 'Bastien', 'Dimitri', 'Luka', 'Djibril', 'Ilan', 'Romeo', 'Olivier', 'Virgile', 'Kaïs', 'Auguste', 'Damien', 'Timothe', 'Noham', 'Alban', 'Bilal', 'Younes', 'Eric', 'Tiago', 'Souleymane', 'Omar', 'Zacharie', 'Eloi', 'Adem', 'Jonas', 'Matthias', 'Timeo', 'Balthazar', 'Ibrahima', 'Jacques', 'Rafaël', 'James', 'Walid', 'Mohammed', 'Karim', 'Aurèle', 'Abdoulaye', 'Julian', 'Anas', 'Adama', 'Yohan', 'Gaël', 'Ousmane', 'Georges', 'Raphael', 'Sam', 'Owen', 'Ambroise', 'Elyes', 'Aymeric', 'Adel', 'Dan', 'Jeremie', 'Cyprien', 'Lino', 'Aymen', 'Ariel', 'Christian', 'Ivan', 'Jean-Baptiste', 'Johan', 'Arnaud', 'Sebastien', 'Philippe', 'Marceau', 'Ange', 'Léo', 'Youssouf', 'Leandre', 'Lilian', 'Gauthier', 'Tony', 'Killian', 'Swann', 'Constantin', 'Alan', 'Mickaël', 'Benoît', 'Idriss', 'Niels', 'Clovis', 'Gaston', 'Marwan', 'Nael', 'Marcus', 'Jad', 'Naïm', 'Elyas', 'Ilyas', 'Ferdinand', 'Florent', 'Malik', 'Yannis', 'Justin', 'Mahamadou', 'Gaëtan', 'Adrian', 'Elio', 'Andy', 'Lukas', 'Karl', 'Aksel', 'Neil', 'Sandro', 'Ernest', 'Oumar', 'Kenzo', 'Sekou', 'Lassana', 'Marco', 'Xavier', 'Melvin', 'Thibaut', 'Robinson', 'Christophe', 'Jordan', 'Melvil', 'Aboubacar', 'Darius', 'Bilel', 'Ewen', 'Ilyès'];
